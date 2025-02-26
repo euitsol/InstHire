6efc58277b68120822464b8cede5e4a97eb3236d
al-kafi-sohag <85807115+al-kafi-sohag@users.noreply.github.com>
2025-02-26 21:32:31 +0600

    required sign added

--- resources/views/institute/setup/stall_option/edit.blade.php
+++ resources/views/institute/setup/stall_option/edit.blade.php
@@ -2,10 +2,10 @@
 @section('title', 'Edit Stall Option')
 @section('content')
     <!-- Stall Option Edit -->
-    <div class="card mb-4">
+    <div class="mb-4 card">
         <div class="card-body">
             <div class="d-flex align-items-center justify-content-between">
-                <h2 class="card-title mb-4">{{ __('Edit Stall Option') }}</h2>
+                <h2 class="mb-4 card-title">{{ __('Edit Stall Option') }}</h2>
                 <a href="{{ route('institute.setup.jfs.list') }}" class="btn btn-sm btn-primary">
                     <i class="bi bi-arrow-left"></i> {{ __('Back') }}
                 </a>
@@ -15,28 +15,28 @@
                 @csrf
                 @method('PUT')
                 <div class="mb-3">
-                    <label for="stall_size" class="form-label">Stall Size</label>
-                    <input type="text" class="form-control" value="{{ old('stall_size', $stallOption->stall_size) }}" 
+                    <label for="stall_size" class="form-label">Stall Size</label><small class="text-danger">*</small>
+                    <input type="text" class="form-control" value="{{ old('stall_size', $stallOption->stall_size) }}"
                         placeholder="Enter stall size" id="stall_size" name="stall_size">
                     @include('alerts.feedback', ['field' => 'stall_size'])
                 </div>
 
                 <div class="mb-3">
-                    <label for="maximum_representative" class="form-label">Maximum Representatives</label>
-                    <input type="number" class="form-control" value="{{ old('maximum_representative', $stallOption->maximum_representative) }}" 
+                    <label for="maximum_representative" class="form-label">Maximum Representatives</label><small class="text-danger">*</small>
+                    <input type="number" class="form-control" value="{{ old('maximum_representative', $stallOption->maximum_representative) }}"
                         placeholder="Enter maximum representatives" id="maximum_representative" name="maximum_representative" min="1">
                     @include('alerts.feedback', ['field' => 'maximum_representative'])
                 </div>
 
                 <div class="mb-3">
                     <label for="description" class="form-label">Description</label>
-                    <textarea class="form-control" id="description" name="description" rows="3" 
+                    <textarea class="form-control" id="description" name="description" rows="3"
                         placeholder="Enter description">{{ old('description', $stallOption->description) }}</textarea>
                     @include('alerts.feedback', ['field' => 'description'])
                 </div>
 
                 <div class="mb-3">
-                    <label for="status" class="form-label">Status</label>
+                    <label for="status" class="form-label">Status</label><small class="text-danger">*</small>
                     <select name="status" id="status" class="form-control">
                         <option value="{{ App\Models\JobFairStallOption::STATUS_ACTIVE }}" {{ old('status', $stallOption->status) == App\Models\JobFairStallOption::STATUS_ACTIVE ? 'selected' : '' }}>Active</option>
                         <option value="{{ App\Models\JobFairStallOption::STATUS_INACTIVE }}" {{ old('status', $stallOption->status) == App\Models\JobFairStallOption::STATUS_INACTIVE ? 'selected' : '' }}>Inactive</option>
