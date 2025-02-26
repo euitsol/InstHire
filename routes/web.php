09477297afb9a3e9cb933e766bbe56b4427d381a
Md Shariful Islam <116838065+devshariful99@users.noreply.github.com>
2025-02-26 14:08:55 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('admin.login');
 })->name('welcome');
 
 Auth::routes();
